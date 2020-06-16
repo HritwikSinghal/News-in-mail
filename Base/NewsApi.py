5e3400aad3ad48d3e27d49f34d639d88ca4f5891
Hritwik <hritwiksinghal@outlook.in>
2020-06-17 05:08:43 +0530

    add news api

---
+++ Base/NewsApi.py
@@ -0,0 +1,87 @@
+import requests
+import json
+from bs4 import BeautifulSoup
+
+
+def getNews(category):
+    newsDictionary = []
+
+    try:
+        if category != 'all':
+            htmlBody = requests.get('https://www.inshorts.com/en/read/' + category)
+        else:
+            htmlBody = requests.get('https://www.inshorts.com/en/read/')
+
+    except requests.exceptions.RequestException as e:
+        newsDictionary['success'] = False
+        newsDictionary['error'] = str(e.message)
+        return newsDictionary
+
+    soup = BeautifulSoup(htmlBody.text, 'html5lib')
+    newsCards = soup.find_all(class_='news-card')
+
+    if not newsCards:
+        newsDictionary['success'] = False
+        newsDictionary['error'] = 'Invalid Category'
+        return newsDictionary
+
+    for index, card in enumerate(newsCards):
+
+        try:
+            readMoreUrl = card.find(class_='read-more').find('a').get('href')
+            if str(readMoreUrl).startswith('https://ad.doubleclick.net/'):
+                continue
+        except AttributeError:
+            readMoreUrl = None
+
+        try:
+            title = card.find(class_='news-card-title').find('a').text
+        except AttributeError:
+            title = None
+
+        # try:
+        #     imageUrl = card.find(
+        #         class_='news-card-image')['style'].split("'")[1]
+        # except AttributeError:
+        #     imageUrl = None
+
+        try:
+            url = ('https://www.inshorts.com' + card.find(class_='news-card-title')
+                   .find('a').get('href'))
+        except AttributeError:
+            url = None
+
+        try:
+            content = card.find(class_='news-card-content').find('div').text
+        except AttributeError:
+            content = None
+
+        # try:
+        #     author = card.find(class_='author').text
+        # except AttributeError:
+        #     author = None
+
+        try:
+            date = card.find(clas='date').text
+        except AttributeError:
+            date = None
+
+        try:
+            time = card.find(class_='time').text
+        except AttributeError:
+            time = None
+
+        newsObject = {
+            'title': str(title).strip(),
+            # 'imageUrl': imageUrl,
+            'url': url,
+            'content': content,
+            # 'author': author,
+            'date': date,
+            'time': time,
+            'readMoreUrl': readMoreUrl
+        }
+
+        newsDictionary.append(newsObject)
+
+    return json.dumps(newsDictionary)
--- Base/main.py
+++ Base/main.py
@@ -1,7 +1,19 @@
 import re
 import os
+import json
 import platform
+from Base import NewsApi
+
+
+def getNews():
+    pass
 
 
 def start(email, psswd, test=0):
     print(email, psswd)
+    news = []
+
+    data = json.loads(NewsApi.getNews('all'))
+    for ele in data:
+        print(ele)
+        print("****************")
